3fef3221a2e18c5719e5ccb8914d63f47eec0ce4
Aleksandr Kondrashov <visitor-infra0l@icloud.com>
2025-03-29 17:30:04 +0900

    bundle dependencies

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
+async function run() {
+  try {
+    const token = core.getInput("github-token");
+    const octokit = github.getOctokit(token);
+
+    const context = github.context;
+    const { owner, repo, number: pull_number } = context.issue;
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pull_number,
+      body: `# Test mesasge!`,
+    });
+  } catch (error) {
+    let errorMessage: string;
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === "string") {
+      errorMessage = error;
+    } else {
+      errorMessage = JSON.stringify(error);
+    }
+
+    core.setFailed(errorMessage);
+  }
+}
+
+run();
